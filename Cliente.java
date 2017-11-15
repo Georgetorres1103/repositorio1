eb5b145067661e8862510b24ebed8bee2793b5ad
Georgetorres1103 <jrt.09@hotmail.com>
2017-11-14 20:52:45 -0500

    Version1

---
+++ Cliente.java
@@ -0,0 +1,14 @@
+package prueba_java;
+
+import javax.swing.JFrame;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        VentanaCliente fram = new VentanaCliente();
+        fram.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+
+
